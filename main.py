15b7f7222fe29bc1196e49090ca895673f435cf6
Guido Delponte <guido_delponte@hotmail.com>
2024-06-26 06:41:11 -0300

    feat: Update main.py to use JSON data for device version comparison
    The `main.py` file has been updated to use JSON data for comparing the device version with the preferred version. This change improves the accuracy of the version comparison and ensures that the device is up to date with the preferred version.

--- main.py
+++ main.py
@@ -7,37 +7,36 @@ from logger import info_logger, error_logger
 from utils import ensure_dir_exists, get_most_recent_file, get_source_dir
 
 
-json_source_dir = get_source_dir('json')
-json_source_dir_exist = ensure_dir_exists(json_source_dir)
+def update_device_with_json(json_file, devices):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
 
-if json_source_dir_exist:
-    json_file = get_most_recent_file(json_source_dir, '.json')
-    if not json_file:
+    for device in devices:
+        model_type = device.identify_model()
+        if model_type:
+            for item in data:
+                if model_type in item:
+                    prefered_version = item[model_type].get(device.sw_version.rsplit('.', 1)[0], [None])[0]
+                    if prefered_version and prefered_version != device.sw_version:
+                        device.sw_version_prefered = prefered_version
+                    else:
+                        device.sw_version_prefered = 'is up to date with prefered version'
+    return devices
+
+
+def main():
+    info_logger.info('Starting the program to collect data from devices')
+    if not get_most_recent_file(get_source_dir('json'), '.json'):
+        info_logger.info('JSON directory does not exist. We try to extract data from HTML tables')
         extract_and_process_html_tables()
-        exit()
+        return
     else:
-        info_logger.info(f'Processing JSON file: {json_file}')
-        devices = collect_data_from_devices()
-        if devices:
-            for device in devices:
-                model_type = device.identify_model()
-                if model_type:
-                    with open(json_file, 'r') as file:
-                        data = json.load(file)
-                    if data: 
-                        for item in data:
-                            if model_type in item:
-                                prefered_version = item[model_type].get(device.sw_version.rsplit('.', 1)[0])[0]
-                                if prefered_version != device.sw_version:
-                                    device.sw_version_prefered = prefered_version
-                                else:
-                                    device.sw_version_prefered = 'is up to date with prefered version'
-                                
-            save_to_excel(devices, 'output.xlsx')  
-
-        else:
-            error_logger.error('No devices were processed.')
-            exit()
-
-
-
+        # devices = collect_data_from_devices()
+        # if devices:
+        #     processed_devices = update_device_with_json(json_file, devices)
+        #     save_to_excel(processed_devices, 'output.xlsx')       
+        print('JSON directory exists')
+        pass
+if __name__ == "__main__":
+    main()
+    
